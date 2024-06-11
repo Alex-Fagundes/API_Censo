e9eb8755b27a043c2e389f06525357a271b700a5
Alex Fagundes <alexfagundes.dev@gmail.com>
2024-06-11 19:54:10 -0300

    commit_4

--- api_censo.py
+++ api_censo.py
@@ -1,10 +1,24 @@
 from flask import Flask
+import json
+import dados_import
 
 
 app = Flask(__name__)
 
+tabela_dados = dados_import.dados_2023
+
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def listar_regiao():
+    regioes = {"regiões": {}}
+    tabela = tabela_dados[["regiao", "sigla_uf", "uf"]]
+    tabela = tabela.drop_duplicates(ignore_index=True)
+    for regiao, dados in tabela.groupby("regiao"):
+        regioes["regiões"][regiao] = {
+            linha["sigla_uf"]: linha["uf"] for i, linha in dados.iterrows()
+        }
+    # regioes = json.dumps(regioes)
+    return regioes
+
 
 app.run()
