ffbee5a7308cf26cc8c86509a8e40464d51c024c
Alex Fagundes <alexfagundes.dev@gmail.com>
2024-06-12 15:55:00 -0300

    commit_5

--- api_censo.py
+++ api_censo.py
@@ -1,24 +1,16 @@
-from flask import Flask
-import json
+from flask import Flask, jsonify
 import dados_import
 
 
 app = Flask(__name__)
 
-tabela_dados = dados_import.dados_2023
+dataframe = dados_import.dados_2023
 
 
-@app.route("/")
-def listar_regiao():
-    regioes = {"regiões": {}}
-    tabela = tabela_dados[["regiao", "sigla_uf", "uf"]]
-    tabela = tabela.drop_duplicates(ignore_index=True)
-    for regiao, dados in tabela.groupby("regiao"):
-        regioes["regiões"][regiao] = {
-            linha["sigla_uf"]: linha["uf"] for i, linha in dados.iterrows()
-        }
-    # regioes = json.dumps(regioes)
-    return regioes
+@app.route("/escola")
+def exibir_dados():
+    dados = dataframe.head(50).to_dict(orient="records")
+    return jsonify({"escolas": dados})
 
 
 app.run()
--- dados_import.py
+++ dados_import.py
@@ -6,14 +6,23 @@ def ler_arquivo(arquivo):
     return arquivo
 
 
-def filtrar_dados(dataframe, colunas):
-    dataframe = dataframe.iloc[:, colunas]
+def filtrar_dados(dataframe, lista_colunas):
+    dataframe = dataframe.iloc[:, lista_colunas]
+    dataframe.drop_duplicates(inplace=True)
     return dataframe
 
 
-def concatenar_df(lista_df):
-    df_concatenado = pd.concat(lista_df, ignore_index=True)
-    return df_concatenado
+def concatenar_df(lista_df, titulo_coluna):
+    for i, df in enumerate(lista_df):
+        df["NOVA_COLUNA"] = f"df{i}"
+        codigo = f"df{i+1}"
+    df_concatenado = pd.concat(lista_df)
+    df_final = df_concatenado.drop_duplicates(subset=titulo_coluna)
+    linhas_repetidas = df_concatenado[
+        df_concatenado.duplicated(keep=False, subset=titulo_coluna)
+    ]
+    df_final.loc[df_final.index.isin(linhas_repetidas.index), "NOVA_COLUNA"] = codigo
+    return df_final
 
 
 def padronizar_cabecalho(dataframe, dict_cabecalho):
@@ -21,16 +30,16 @@ def padronizar_cabecalho(dataframe, dict_cabecalho):
     return dataframe
 
 
-def substituir_codigos(dataframe, coluna, dict_codigos):
-    dataframe[coluna] = dataframe[coluna].replace(dict_codigos)
+def substituir_codigos(dataframe, titulo_coluna, dict_codigos):
+    dataframe[titulo_coluna] = dataframe[titulo_coluna].replace(dict_codigos)
     return dataframe
 
 
 arquivo_ed_basica = "microdados_ed_basica_2023.csv"
 arquivo_tecnicos = "suplemento_cursos_tecnicos_2023.csv"
 
-colunas_ed_basica_2023 = [0, 1, 3, 4, 6, 17, 19]
-colunas_tecnicos_2023 = [0, 1, 3, 4, 6, 10, 11]
+colunas_ed_basica_2023 = [0, 1, 3, 4, 6, 17, 18, 19]
+colunas_tecnicos_2023 = [0, 1, 3, 4, 6, 10, 11, 12]
 
 novo_cabecalho = {
     "NU_ANO_CENSO": "ano_censo",
@@ -39,13 +48,20 @@ novo_cabecalho = {
     "SG_UF": "sigla_uf",
     "NO_MUNICIPIO": "municipio",
     "NO_ENTIDADE": "entidade",
+    "CO_ENTIDADE": "codigo_entidade",
     "TP_DEPENDENCIA": "administracao",
+    "NOVA_COLUNA": "nivel",
 }
 depen_adm = {
     1: "federal",
     2: "estadual",
     3: "municipal",
     4: "privada",
+}
+niveis_ensino = {
+    "df0": "básico",
+    "df1": "técnico",
+    "df2": "básico e técnico",
 }
 
 ed_basica_2023 = ler_arquivo(arquivo_ed_basica)
@@ -54,8 +70,9 @@ tecnicos_2023 = ler_arquivo(arquivo_tecnicos)
 ed_basica_2023 = filtrar_dados(ed_basica_2023, colunas_ed_basica_2023)
 tecnicos_2023 = filtrar_dados(tecnicos_2023, colunas_tecnicos_2023)
 
-dados_2023 = concatenar_df([ed_basica_2023, tecnicos_2023])
+dados_2023 = concatenar_df([ed_basica_2023, tecnicos_2023], "CO_ENTIDADE")
 
 dados_2023 = padronizar_cabecalho(dados_2023, novo_cabecalho)
 
 dados_2023 = substituir_codigos(dados_2023, "administracao", depen_adm)
+dados_2023 = substituir_codigos(dados_2023, "nivel", niveis_ensino)
